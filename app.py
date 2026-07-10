be126c13391c55272e66d31b2e503bc37c7310ec
Lokesh kumar S <sit23cs078@sairamtap.edu.in>
2026-07-10 19:46:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -510,7 +510,7 @@ def add_customers():
             "company_type": data.get("ctype"),
             "contact_name": data.get("ccname"),
             "cantact_mail": data.get("cmail"),
-            "contact_phone": data.get("cphone")
+            "contact_phone": data.get("phone")
         }).execute()
 
         return jsonify({"message": "Customer added successfully"})
